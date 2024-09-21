a72264441f7129b0101f86d0d8fa047eb267748a
Michael-Chacon <michaelchacon02@gmail.com>
2024-09-20 19:59:11 -0500

    feat: :sparkles: Fix class ResourceNotFoundException.

--- src/main/java/com/app/app/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/app/app/exceptions/ResourceNotFoundException.java
@@ -5,6 +5,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class ResourceNotFoundException extends ResponseStatusException {
     public ResourceNotFoundException(String entityName, Long id){
-        super(HttpStatus.NOT_FOUND, String.format("Not results found for %s with id: %s", entityName, id));
+        super(HttpStatus.NOT_FOUND, String.format("Not results found for table %s with id: %s", getNameClass(entityName), id));
+    }
+
+    /*
+    * Creé este método porque olvidé utilizar getSimpleName() de la clase, y en su lugar usé getName(),
+    * lo cual devuelve la ruta completa de la clase.
+    * */
+    public static String getNameClass(String path){
+        int getLastPoint = path.lastIndexOf(".");
+        return path.substring(getLastPoint + 1);
     }
 }
